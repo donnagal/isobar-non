acbacceb36a0e3c43ce4acd3b6f31c6f1bb61660
Donna Galletta <donna.galletta@isobar.com>
2018-06-28 12:39:46 +1000

    add lazy loaded image

--- js/creative.js
+++ js/creative.js
@@ -1,3 +1,163 @@
+AOS.init({
+  easing: 'ease-out-back',
+  duration: 1000
+});
 
-AOS.init({easing:"ease-out-back",duration:1e3}),function(e){e(".js-scroll-trigger").click(function(){e(".navbar-collapse").collapse("hide")});new Swiper("#galleryIntro",{slidesPerView:"1",speed:1,centeredSlides:!0,autoplay:!0,effect:"fade"}),new Swiper("#gallery2018",{slidesPerView:"auto",centeredSlides:!0,spaceBetween:14,keyboard:{enabled:!0,onlyInViewport:!1},pagination:{el:".pag2018",type:"fraction"},navigation:{nextEl:".gallery2018-b",prevEl:".gallery2018-a"}}),new Swiper("#gallery2017",{slidesPerView:"auto",centeredSlides:!0,spaceBetween:14,keyboard:{enabled:!0,onlyInViewport:!1},pagination:{el:".pag2017",type:"fraction"},navigation:{nextEl:".gallery2017-b",prevEl:".gallery2017-a"}}),new Swiper("#gallery2016",{slidesPerView:"auto",centeredSlides:!0,spaceBetween:14,keyboard:{enabled:!0,onlyInViewport:!1},pagination:{el:".pag2016",type:"fraction"},navigation:{nextEl:".gallery2016-b",prevEl:".gallery2016-a"}}),new Swiper("#gallery2015",{slidesPerView:"auto",centeredSlides:!0,spaceBetween:14,keyboard:{enabled:!0,onlyInViewport:!1},pagination:{el:".pag2015",type:"fraction"},navigation:{nextEl:".gallery2015-b",prevEl:".gallery2015-a"}}),new Swiper("#gallery2014",{slidesPerView:"auto",centeredSlides:!0,spaceBetween:14,keyboard:{enabled:!0,onlyInViewport:!1},pagination:{el:".pag2014",type:"fraction"},navigation:{nextEl:".gallery2014-b",prevEl:".gallery2014-a"}}),new Swiper("#gallery2013",{slidesPerView:"auto",centeredSlides:!0,spaceBetween:14,keyboard:{enabled:!0,onlyInViewport:!1},pagination:{el:".pag2013",type:"fraction"},navigation:{nextEl:".gallery2013-b",prevEl:".gallery2013-a"}})}(jQuery),$(".video").parent().click(function(){$(this).children(".video").get(0).paused?($(this).children(".video").get(0).play(),$(this).children(".playpause").fadeOut()):($(this).children(".video").get(0).pause(),$(this).children(".playpause").fadeIn())}),$(".video").hover(function(){this.hasAttribute("controls")?this.removeAttribute("controls"):this.setAttribute("controls","controls")});
+(function($) {
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+  });
+
+
+  //idangero.us/swiper/api/
+
+  var swiper = new Swiper('#galleryIntro', {
+      lazy: true,
+      slidesPerView: '1',
+      speed: 1,
+      centeredSlides: true,
+      autoplay: true,
+      effect: 'fade',
+  });
+
+  var swiper = new Swiper('#gallery2018', {
+      lazy: true,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 14,
+      keyboard: {
+          enabled: true,
+          onlyInViewport: false,
+      },
+      pagination: {
+          el: '.pag2018',
+          type: 'fraction',
+      },
+      navigation: {
+          nextEl: '.gallery2018-b',
+          prevEl: '.gallery2018-a',
+      },
+  });
+
+  var swiper = new Swiper('#gallery2017', {
+      lazy: true,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 14,
+      keyboard: {
+          enabled: true,
+          onlyInViewport: false,
+      },
+      pagination: {
+          el: '.pag2017',
+          type: 'fraction',
+      },
+      navigation: {
+          nextEl: '.gallery2017-b',
+          prevEl: '.gallery2017-a',
+      },
+  });
+
+  var swiper = new Swiper('#gallery2016', {
+      lazy: true,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 14,
+      keyboard: {
+          enabled: true,
+          onlyInViewport: false,
+      },
+      pagination: {
+          el: '.pag2016',
+          type: 'fraction',
+      },
+      navigation: {
+          nextEl: '.gallery2016-b',
+          prevEl: '.gallery2016-a',
+      },
+  });
+
+
+  var swiper = new Swiper('#gallery2015', {
+      lazy: true,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 14,
+      keyboard: {
+          enabled: true,
+          onlyInViewport: false,
+      },
+      pagination: {
+          el: '.pag2015',
+          type: 'fraction',
+      },
+      navigation: {
+          nextEl: '.gallery2015-b',
+          prevEl: '.gallery2015-a',
+      },
+  });
+
+
+  var swiper = new Swiper('#gallery2014', {
+      lazy: true,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 14,
+      keyboard: {
+          enabled: true,
+          onlyInViewport: false,
+      },
+      pagination: {
+          el: '.pag2014',
+          type: 'fraction',
+      },
+      navigation: {
+          nextEl: '.gallery2014-b',
+          prevEl: '.gallery2014-a',
+      },
+  });
+
+
+  var swiper = new Swiper('#gallery2013', {
+      lazy: true,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 14,
+      keyboard: {
+          enabled: true,
+          onlyInViewport: false,
+      },
+      pagination: {
+          el: '.pag2013',
+          type: 'fraction',
+      },
+      navigation: {
+          nextEl: '.gallery2013-b',
+          prevEl: '.gallery2013-a',
+      },
+  });
+
+})(jQuery);
+
+// Video player, play and pause 
+$('.video').parent().click(function() {
+  if ($(this).children(".video").get(0).paused) {
+      $(this).children(".video").get(0).play();
+      $(this).children(".playpause").fadeOut();
+  } else {
+      $(this).children(".video").get(0).pause();
+      $(this).children(".playpause").fadeIn();
+  }
+});
+
+// Video player, show controls on hover 
+$('.video').hover(function toggleControls() {
+  if (this.hasAttribute("controls")) {
+      this.removeAttribute("controls")
+  } else {
+      this.setAttribute("controls", "controls")
+  }
+})
 
